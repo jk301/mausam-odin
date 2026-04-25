137a890a3e29ce6dc2e9161e682661491dc3956a
junaid <jk7948677@gmail.com>
2026-04-25 23:51:17 +0530

    Added header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 /* index.js */
 import "./styles.css";
 
-console.log("helo");
+const mainTab = document.createElement("div")
+mainTab.classList.add("main-tab")
+document.body.appendChild(mainTab)
+
+// The header
+const header = (() => {
+    const headerDiv = document.createElement("div")
+    headerDiv.classList.add("main-header")
+    const title = document.createElement("h2")
+    title.textContent = "MAUSAM"
+
+    const search = document.createElement("input")
+    search.type = "search"
+    search.placeholder = "search"
+
+    headerDiv.appendChild(title)
+    headerDiv.appendChild(search)
+
+    return { headerDiv, search }
+})()
+
+mainTab.appendChild(header.headerDiv)
+
+header.search.addEventListener("input", () => {console.log("input")})
